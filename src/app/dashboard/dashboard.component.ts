e08912b35d745298db307e6813ec494c35d86b46
Your Name <msksurjith@gmail.com>
2025-01-22 17:49:09 +0530

    done

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
